f98f2dba5f894425e41439828784a7001c94751b
Nikshan0702 <nikshanps07@gmail.com>
2025-06-24 15:58:07 +0530

    Controllers Updated

---
+++ Backend/Server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv"; 
+import UserOperations from './Controllers/UserOperations.js';
+
+
+dotenv.config();
+const app = express();
+
+app.use('/uploads', express.static('public/uploads'));
+
+
+
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:5173'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+app.options('*', cors());
+app.use(express.json()); 
+
+
+app.use("/UserOperations", UserOperations);
+
+
+
+mongoose.connect(process.env.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.log("MongoDB connection error:", err));
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
